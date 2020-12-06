5af8b7277b80c7841f437257048f69f569edf744
Jose Sedrez <jose.sedrez@melhorenvio.com>
2020-12-06 19:45:11 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@
 Route::get('/', function () {
     $users = \App\User::all();
 
+    dd(parse_url("postgres://rheokifiiodlwe:2162e299ab49adce86f93cf16e9a6bd0cf95393e114d326cf6d885469ab3b7af@ec2-52-70-15-120.compute-1.amazonaws.com:5432/dcq9jdf0qicisd"));
+
     return view('welcome', compact('users'));
 });
